680b64ea910cf0f07b812719c1bfd826cb1f8bee
Xheneta Hysenlekaj <xhe_neta@live.com>
2021-05-04 16:47:37 +0200

    RoleBLL class is created

---
+++ PVCWindowsStudio.BLL/RoleBLL.cs
@@ -0,0 +1,50 @@
+﻿using PVCWindowsStudio.BO;
+using PVCWindowsStudio.BO.Interfaces;
+using PVCWindowsStudio.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVCWindowsStudio.BLL
+{
+    public class RoleBLL : IRepository<Roles>
+    {
+        private readonly RoleDAL dal = new RoleDAL();
+        public bool Delete(int id)
+        {
+            return dal.Delete(id);
+        }
+
+        public bool Delete(Roles model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Roles Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Roles Get(Roles model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Roles> GetAll()
+        {
+            return dal.GetAll();
+        }
+
+        public bool Insert(Roles model)
+        {
+            return dal.Insert(model);
+        }
+
+        public bool Update(Roles model)
+        {
+            return dal.Update(model);
+        }
+    }
+}
